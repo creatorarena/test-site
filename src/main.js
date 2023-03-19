59f81ff1d2f64dfc88ee2e79b7f0b4eb7eb0d839
creatorarena <creatorarena@gmail.com>
2023-03-19 23:59:18 +0900

    study day2

--- src/main.js
+++ src/main.js
@@ -1,53 +1,63 @@
-'use strict';
+"use strict";
 
 //* DOM이 다 로딩되고 실행할 함수
-window.addEventListener('DOMContentLoaded', () => {
-    let cid = localStorage.getItem('cid')
-    if(cid) {
-        document.querySelector('h1').replaceChildren(`${cid} Welcome`)
-        alert(`Hello! ${cid}`)
-    }
-})
+window.addEventListener("DOMContentLoaded", () => {
+  let cid = localStorage.getItem("cid");
+  if (cid) {
+    document.querySelector("h1").replaceChildren(`${cid} Welcome`);
+    alert(`Hello! ${cid}`);
+  }
+});
 
+let myHeading = document.querySelector("h1");
 
-let myHeading = document.querySelector('h1');
-
-myHeading.textContent = 'Hello world!'
+myHeading.textContent = "Hello world!";
 
 function multiply(num1, num2) {
-    return num1 * num2
+  return num1 * num2;
 }
 
 function clickEventHandler() {
-    alert('Ouch! Stop poking me!')
+  alert("Ouch! Stop poking me!");
 }
-
 
 //* 경고 버튼
-document.querySelector('.beforeBTN').onclick = () => { alert('Ouch! Stop poking me! ') }
+document.querySelector(".beforeBTN").onclick = () => {
+  alert("Ouch! Stop poking me! ");
+};
 
 //* 이미지 변경 버튼
-document.querySelector('.imgChangerBTN').onclick = () => {
-    let img = document.querySelector('img')
-    let changedImg = img.getAttribute('src') == 'assets/homeImage.jpg' ? 'assets/changeImg.jpg' : 'assets/homeImage.jpg'
-    img.setAttribute('src', changedImg)
-}
+document.querySelector(".imgChangerBTN").onclick = () => {
+  let img = document.querySelector("img");
+  let changedImg =
+    img.getAttribute("src") == "assets/homeImage.jpg"
+      ? "assets/changeImg.jpg"
+      : "assets/homeImage.jpg";
+  img.setAttribute("src", changedImg);
+};
 
 //* 로그인 버튼
-document.querySelector('.loginBTN').onclick = () => {
-    if(!localStorage.getItem('cid')) {
-        let str = prompt();
-        str != null ? localStorage.setItem('cid', str) : prompt();
-    }
-    else {
-        alert(`로그인 되어 있습니다 ${localStorage.getItem('cid')}`)
-    }
-}
+document.querySelector(".loginBTN").onclick = () => {
+  if (!localStorage.getItem("cid")) {
+    let str = prompt();
+    str != null ? localStorage.setItem("cid", str) : prompt();
+  } else {
+    alert(`로그인 되어 있습니다 ${localStorage.getItem("cid")}`);
+  }
+};
 
 //* 캐쉬 클리어 버튼
-document.querySelector('.clearBTN').onclick = () => {
-    localStorage.clear()
-    alert('cleared!')
-}
+document.querySelector(".clearBTN").onclick = () => {
+  localStorage.clear();
+  alert("cleared!");
+};
 // Storage.setItem()
 
+document.querySelector(".add-thinker").onclick = () => {
+  const name = prompt();
+  const ul = document.querySelector(".thinker-list");
+  const li = document.createElement("li");
+  const textNode = document.createTextNode(name);
+  li.appendChild(textNode);
+  ul.appendChild(li);
+};
